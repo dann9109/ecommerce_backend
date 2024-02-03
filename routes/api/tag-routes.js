f4aac12146adc7a609bdf0fb3947b82544628857
dann9109 <danielxstella@gmail.com>
2024-02-03 11:07:02 -0500

    seed

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../models');
+const { Tag, Product, ProductTag } = require('../../models');
 
 // GET all tags
 router.get('/', async (req, res) => {
